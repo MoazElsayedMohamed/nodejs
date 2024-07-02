3daf5dbd7aeb89ef4d16f96351030101dbd644b5
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-02 13:25:40 +0300

    feat: :sparkles: set model view controller to express project

---
+++ Express-Project/models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+  {
+    id: 0,
+    name: "sir Isaac",
+  },
+  {
+    id: 1,
+    name: "sir albert",
+  },
+];
+
+module.exports = friends;
--- Express-Project/server.js
+++ Express-Project/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const frinedscontroller = require("./controllers/friends.controller");
+const messagesController = require("./controllers/messages.controller");
 const app = express();
 const port = 8888;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
 });
+
+app.use(express.json());
+
+app.post("/friends", frinedscontroller.postFriends);
+app.get("/friends", frinedscontroller.getFriends);
+app.get("/friends/:friendId", frinedscontroller.getFriend);
+
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessages);
 
 app.listen(port, () => console.log(`Listening on ${port}`));
